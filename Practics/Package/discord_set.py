cf2c8408b4e1b2824388eb2948b041150e3dd05a
xzawed <dirtchamber@naver.com>
2022-04-19 00:08:48 +0900

    COMMIT AND PUSH

--- Practics/Package/discord_set.py
+++ Practics/Package/discord_set.py
@@ -1,2 +1,44 @@
 
+import pymysql
+#  호출부 구현
+#  class개념
+class MariaDB:
+    #  MariaDB setting
+    def sessionmysql(self):
+        self.db = pymysql.connect(
+                                    host='dirtchamber.iptime.org',
+                                    port=13306,
+                                    user='root',
+                                    password='root',
+                                    db='DEV_MARIADB',
+                                    charset='utf8',
+                                    autocommit=False,
+                                    cursorclass=pymysql.cursors.DictCursor
+                                 )
+        #  DB와 관련된 커서 객체를 생성한다
+        self.curs = self.db.cursor()
+
+    #  MariaDB 연결이후 SELECT, UPDATE, INSERT, DELETE 에 해당되는 내용을 호출처리
+    def endmysql(self):
+        sql = "SELECT 'A';"
+        self.curs.execute(sql)
+        print("정상적으로 종료 되었습니다.")
+
+    #  MariaDB Close
+    def closemysql(self):
+        self.curs.close()
+########################################################################################################################
+
+########################################################################################################################
+#  전역함수처리(외부에서 호출할때 쓰임)
+def selmysql(opt):
+        MariaDB.sessionmysql(self=MariaDB)
+
+        if   opt == "TOKEN":
+            MariaDB.endmysql(self=MariaDB)
+
+        MariaDB.closemysql(self=MariaDB)
+########################################################################################################################
+
+#  discord에서는 token정보가 직접노출되면 난리침..다른 방법을 사용해야함.
 xzawed_token = "OTY1MTc5ODUyMzIxODgyMTMy.YlvbyA.TQETCCRsuwYCD-VAXioi7duDGeI"
--- main.py
+++ main.py
@@ -1,41 +1,65 @@
-# 샘플 Python 스크립트입니다.
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
-#def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
+#  샘플 Python 스크립트입니다.
+#  Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
+#  클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+#  def print_hi(name):
+    #  스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
 #    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+#  스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+#  if __name__ == '__main__':
+#      print_hi('PyCharm')
+#   https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
 import discord
 import discord_set
 
 discord_token = discord_set.xzawed_token
 
-# discord Client class를 생성합니다.
+#  discord Client class를 생성합니다.
 client = discord.Client()
 
-# event decorator를 설정하고 on_ready function을 할당해줍니다.
+#  event decorator를 설정하고 on_ready function을 할당해줍니다.
 @client.event
 async def on_ready():  # on_ready event는 discord bot이 discord에 정상적으로 접속했을 때 실행됩니다.
     print('We have logged in as {}'.format(client))
     print('Bot name: {}'.format(client.user.name))  # 여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
     print('Bot ID: {}'.format(client.user.id))  # 여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
 
-# event decorator를 설정하고 on_message function을 할당해줍니다.
+#  event decorator를 설정하고 on_message function을 할당해줍니다.
 @client.event
 async def on_message(message):
-    # message란 discord 채널에 올라오는 모든 message를 의미합니다.
-    # 따라서 bot이 보낸 message도 포함이되죠.
-    # 아래 조건은 message의 author가 bot(=clinet.user)이라면 그냥 return으로 무시하라는 뜻입니다.
+    #  message란 discord 채널에 올라오는 모든 message를 의미합니다.
+    #  따라서 bot이 보낸 message도 포함이되죠.
+    #  아래 조건은 message의 author가 bot(=clinet.user)이라면 그냥 return으로 무시하라는 뜻입니다.
     if message.author == client.user:
         return
 
-    # message를 보낸 사람이 bot이 아니라면 message가 hello로 시작하는 경우 채널에 Hello!라는 글자를 보내라는 뜻입니다.
+    #  message를 보낸 사람이 bot이 아니라면 message가 hello로 시작하는 경우 채널에 Hello!라는 글자를 보내라는 뜻입니다.
     elif message.content.startswith('hello'):
         await message.channel.send('Hello!')
+
+
+#  command를 이용하여 동작하는것을 구현
+@client.command()
+#  The name of the function is the name of the command
+async def square(ctx, arg):
+    #  this is the text that follows the command
+    print(arg)
+    #  ctx.send sends text in chat
+    await ctx.send(int(arg) ** 2)
+
+@client.command()
+async def scrabblepoints(ctx, arg):
+    # Key for point values of each letter
+    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+    points = 0
+    # Sum the points for each letter
+    for c in arg:
+        points += score[c]
+    await ctx.send(points)
 
 # 위에서 설정한 client class를 token으로 인증하여 실행합니다.
 client.run(discord_token)
