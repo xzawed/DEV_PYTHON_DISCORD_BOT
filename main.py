ff42bcb4fd878b89aa806099bd4a5961e8d44c5f
xzawed <dirtchamber@naver.com>
2022-04-24 21:07:08 +0900

    COMMIT AND PUSH

--- main.py
+++ main.py
@@ -9,7 +9,8 @@
 #  if __name__ == '__main__':
 #      print_hi('PyCharm')
 #   https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
-import discord
+import asyncio, discord
+from discord.ext import commands
 
 import mariadb
 import traceback
@@ -17,67 +18,61 @@ import Errlog
 
 discord_token = mariadb.selmysql('TOKEN',('DISCORD','XZAWED#7332'))
 
-#  discord Client class를 생성합니다.
-client = discord.Client()
+bot = commands.Bot(command_prefix="$")
 
 #  event decorator를 설정하고 on_ready function을 할당해줍니다.
 #  on_ready event는 discord bot이 discord에 정상적으로 접속했을 때 실행됩니다.
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {}'.format(client))
+    print('We have logged in as {}'.format(bot))
     #  여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
-    print('Bot name: {}'.format(client.user.name))
+    print('Bot name: {}'.format(bot.user.name))
     #  여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
-    print('Bot ID: {}'.format(client.user.id))
+    print('Bot ID: {}'.format(bot.user.id))
 
 #  event decorator를 설정하고 on_message function을 할당해줍니다.
-@client.event
+'''
+@bot.event
 async def on_message(message):
     #  message란 discord 채널에 올라오는 모든 message를 의미합니다.
     #  따라서 bot이 보낸 message도 포함이되죠.
     #  아래 조건은 message의 author가 bot(=clinet.user)이라면 그냥 return으로 무시하라는 뜻입니다.
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     #  message를 보낸 사람이 bot이 아니라면 message가 hello로 시작하는 경우 채널에 Hello!라는 글자를 보내라는 뜻입니다.
     elif message.content.startswith('hello'):
         await message.channel.send('Hello!')
-
-
-
-#  command를 이용하여 동작하는것을 구현
 '''
+#  커맨드별로 전부 다 선언을 해야 하는지 확인이 필요함.
+@bot.command()
+async def hello(ctx):
+    await ctx.send("안녕하세요")
 
-@client.message_command()
-async def square(ctx, arg):
-    #  this is the text that follows the command
-    print(arg)
-    #  ctx.send sends text in chat
-    await ctx.send(int(arg) ** 2)
+#  discord 에서는 한글 커맨드처리가 가능하다.
+@bot.command()
+async def 구글(ctx, *args):
+    parameter = '+'.join(args)
+    url = "https://www.google.com/search?q="
+    await ctx.reply(f'구글 검색 : {url+parameter}')
 
-@client.message_command()
-async def scrabblepoints(ctx, arg):
-    #  Key for point values of each letter
-    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
-         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
-         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
-         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
-             
-         "x": 8, "z": 10}
-    points = 0
-    #  Sum the points for each letter
-    for c in arg:
-        points += score[c]
-    await ctx.send(points)
+@bot.command()
+async def 네이버(ctx, *args):
+    parameter = '+'.join(args)
+    url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="
+    await ctx.reply(f'네이버 검색 : {url+parameter}')
 
-'''
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.channel.send('명령어를 찾지 못했습니다')
 
 # 위에서 설정한 client class를 token으로 인증하여 실행합니다.
 try:
-    client.run(discord_token)
     mariadb.selmysql('TEMP', '실행')
     err = traceback.format_exc()
     Errlog.saveLog('INFO', str(err))
+    bot.run(discord_token)
 except Exception :
     err = traceback.format_exc()
     Errlog.saveLog('ERROR', str(err))
