19971aed898f5e9c0fd702c786ad47809eee0a4f
xzawed <dirtchamber@naver.com>
2022-05-08 21:53:14 +0900

    COMMIT AND PUSH

--- Practics/Package/Errlog.py
+++ Practics/Package/Errlog.py
@@ -1,11 +1,12 @@
+#  log 기록
+import logging
 
-import logging # log기록
+from mariadb import *
 
-import mariadb
 import traceback
 
 ########################################################################################################################
-#  전역변수
+#  전역 변수
 #  log 설정
 Log = logging.getLogger('DEV_PYTHON_DICORD_LOG')
 
@@ -15,11 +16,11 @@ LogFileName = './Practics/log/DEV_PYTHON_DISCORD_Log.log'
 #  LogFormat = logging.Formatter('[%(process)d | %(thread)d | %(levelname)s | %(filename)s:%(lineno)s] %(asctime)s: %(message)s')
 LogFormat = logging.Formatter('DISCORD|[%(process)d | %(thread)d | %(filename)s:%(lineno)s] %(asctime)s: %(message)s')
 
-#  Console = 콘솔화면에 출력
+#  Console = 콘솔 화면에 출력
 ConsoleHandler = logging.StreamHandler()
 ConsoleHandler.setFormatter(LogFormat)
 
-#  File = 파일에 저장처리
+#  File = 파일에 저장 처리
 FileHandler = logging.FileHandler(LogFileName)
 FileHandler.setFormatter(LogFormat)
 
@@ -31,11 +32,19 @@ Log.addHandler(FileHandler)
 
 ########################################################################################################################
 #  호출부 구현
-def saveLog(state):
+
+
+def savelog(state):
+
     err = traceback.format_exc()
+
     if   state == "INFO":
         Log.info('MESSAGE : ' + str(err))
     elif state == "ERROR":
-        Log.error('MESSAGE : '+ str(err))
-    mariadb.selmysql('LOG', (state, str(err)))
+        Log.error('MESSAGE : ' + str(err))
+
+    db = MYSQL
+    db.selmysql(self=db, opt='LOG', data=(state, str(err)))
+    print(state + " : " + str(err))
+
 ########################################################################################################################
--- Practics/Package/mariadb.py
+++ Practics/Package/mariadb.py
@@ -1,11 +1,18 @@
-import json
 
 import pymysql
+#  개별적 으로 생성한 내용
+import Errlog
 #  호출부 구현
-#  class개념
+#  class 개념
+
 
 class MariaDB:
+
+    sql = ""
+    db = type(None)
+    curs = type(None)
     #  MariaDB setting
+
     def sessionmysql(self):
         self.db = pymysql.connect(
                                     host='xzawed.iptime.org',
@@ -17,34 +24,35 @@ class MariaDB:
                                     autocommit=False,
                                     cursorclass=pymysql.cursors.DictCursor
                                  )
-        #  DB와 관련된 커서 객체를 생성한다
+        #  DB와 관련된 커서 객체를 생성 한다
         self.curs = self.db.cursor()
         #  self.curs = self.db.cursor()
 
-    #  MariaDB 연결이후 SELECT, UPDATE, INSERT, DELETE 에 해당되는 내용을 호출처리
+    #  MariaDB 연결 이후 SELECT, UPDATE, INSERT, DELETE 에 해당 되는 내용을 호출 처리
     def tempmysql(self, data):
-        sql = "SELECT %s RESULT;"
-        self.curs.execute(sql, data)
+        self.sql = "SELECT %s RESULT;"
+        self.curs.execute(self.sql, data)
         result = self.curs.fetchall()
+        message = ""
         for x in result:
             message = x['RESULT']
 
-        print("정상적으로 "+message+" 되었습니다.")
+        print("정상적 으로 " + message + " 되었 습니다.")
 
-    #  MariaDB 연결이후 SELECT, UPDATE, INSERT, DELETE 에 해당되는 내용을 호출처리
+    #  MariaDB 연결 이후 SELECT, UPDATE, INSERT, DELETE 에 해당 되는 내용을 호출 처리
     def tokenmysql(self, data):
-        sql = "SELECT TOKEN FROM BOT_TOKEN WHERE COMCD = %s AND BOT_ID = %s; "
-        self.curs.execute(sql, data)
+        self.sql = "SELECT TOKEN FROM BOT_TOKEN WHERE COMCD = %s AND BOT_ID = %s; "
+        self.curs.execute(self.sql, data)
         token_list = self.curs.fetchall()
-
+        xzawed_token = ""
         for x in token_list:
             xzawed_token = x['TOKEN']
 
         return xzawed_token
 
     def logmysql(self, data):
-        sql = "INSERT INTO BOT_LOG ( SEQ, WRITE_DATE, STATE, LOG, COMCD ) VALUES ( nextval(JOB_LOG_SEQ), SYSDATE(), %s, %s, 'DISCORD' ) "
-        self.curs.execute(sql, data)
+        self.sql = "INSERT INTO BOT_LOG ( SEQ, WRITE_DATE, STATE, LOG, COMCD ) VALUES ( nextval(JOB_LOG_SEQ), SYSDATE(), %s, %s, 'DISCORD' ) "
+        self.curs.execute(self.sql, data)
         self.db.commit()
 
     def closemysql(self):
@@ -53,19 +61,27 @@ class MariaDB:
 ########################################################################################################################
 
 ########################################################################################################################
-#  전역함수처리(외부에서 호출할때 쓰임)
-def selmysql(opt, data):
-        result = ""
-        MariaDB.sessionmysql(MariaDB)
+#  전역 함수 처리( 외부 에서 호출 할 때 쓰임)
 
-        if   opt == "TEMP":
-            MariaDB.tempmysql(MariaDB, data)
-        elif opt == "TOKEN":
-            result = MariaDB.tokenmysql(MariaDB, data)
-        elif opt == "LOG":
-            MariaDB.logmysql(MariaDB, data)
 
-        MariaDB.closemysql(MariaDB)
+class MYSQL(MariaDB):
 
-        return result
+    def selmysql(self, opt, data):
+        try:
+            result = ""
+            super(MYSQL, self).sessionmysql(self)
+            #  print("정상적 으로 MariaDB에 연결 되었 습니다.")
+            if   opt == "TEMP":
+                super(MYSQL, self).tempmysql(self, data)
+            elif opt == "TOKEN":
+                result = super(MYSQL, self).tokenmysql(self, data)
+            elif opt == "LOG":
+                super(MYSQL, self).logmysql(self, data)
+
+            super(MYSQL, self).closemysql(self)
+            #  print("정상적 으로 MariaDB에 연결 해제 되었 습니다.")
+
+            return result
+        except Exception:
+            Errlog.savelog('ERROR')
 ########################################################################################################################
